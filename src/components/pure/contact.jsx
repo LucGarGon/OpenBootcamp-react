8dd33f6ffd3bd6a6d71ac01dddc20af54e75a7b9
lucas <lugarcai@asjdfl>
2022-04-10 17:56:30 +0200

    introducción de useState

--- src/components/pure/contact.jsx
+++ src/components/pure/contact.jsx
@@ -3,16 +3,19 @@ import PropTypes from 'prop-types'
 import { Contacto } from '../../models/contacto.class'
 
 function Contact({contact}) {
+
+  const [conectado, setConectado] = useState(contact.conectado)
   const isConectado = (conectado) => {
     return conectado ? 'Contacto en línea' : 'Contacto No disponible'
   }
+
   return (
     <div>
         <p>nombre: {contact.nombre}</p>
         <p>apellido: {contact.apellido}</p>
         <p>email: {contact.email}</p>
-        <p>conectado: {isConectado(contact.conectado)}</p>
-
+        <p>conectado: {isConectado(conectado)}</p>
+        <button onClick={() => setConectado(!conectado)}>{isConectado(!conectado)}</button>
     </div>
   )
 }
